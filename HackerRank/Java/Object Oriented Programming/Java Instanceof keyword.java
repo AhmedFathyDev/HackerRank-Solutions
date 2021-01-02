f11a4de26538447de71d421bf5ccc8158bb7ff0c
ahmmohfat18 <ahmmohfat18@gmail.com>
2021-01-02 16:12:35 +0200

    Modify "Java Instanceof keyword.java"

--- HackerRank/Java/Object Oriented Programming/Java Instanceof keyword.java
+++ HackerRank/Java/Object Oriented Programming/Java Instanceof keyword.java
@@ -15,7 +15,7 @@ public class InstanceOFTutorial {
         int a = 0, b = 0, c = 0;
         for (int i = 0; i < mylist.size(); i++) {
             Object element = mylist.get(i);
-
+            
             if (element instanceof Student)
                 a++;
             if (element instanceof Rockstar)
