862e384b963623870b7f7a630ec52ceb8d76c88d
ahmmohfat18 <ahmmohfat18@gmail.com>
2021-01-05 11:12:26 +0200

    Solve the first two problems of JavaScript.

---
+++ HackerRank/10 Days of Javascript/Day 0 Data Types.js
@@ -0,0 +1,12 @@
+
+function performOperation(secondInteger, secondDecimal, secondString) {
+
+    const firstInteger = 4;
+    const firstDecimal = 4.0;
+    const firstString = 'HackerRank ';
+
+    console.log(firstInteger + parseInt(secondInteger));
+    console.log(firstDecimal + parseFloat(secondDecimal));
+    console.log(firstString + secondString);
+
+}
