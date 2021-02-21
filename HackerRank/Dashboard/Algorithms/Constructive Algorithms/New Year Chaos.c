5979d587aefa7a1c5e336bb123bc9ee632b3dad5
ahmmohfat18 <ahmmohfat18@gmail.com>
2021-02-21 19:01:00 +0200

    Add "New Year Chaos"'s solution.

---
+++ HackerRank/Dashboard/Algorithms/Constructive Algorithms/New Year Chaos.c
@@ -0,0 +1,35 @@
+
+void minimumBribes(int q_count, int *q)
+{
+    int ans = 0;
+
+    int first = 1, second = 2, third = 3;
+
+    for (size_t i = 0; i < q_count; i++)
+    {
+        if (q[i] == first)
+        {
+            first = second;
+            second = third;
+            ++third;
+        }
+        else if (q[i] == second)
+        {
+            ++ans;
+            second = third;
+            ++third;
+        }
+        else if (q[i] == third)
+        {
+            ans += 2;
+            ++third;
+        }
+        else
+        {
+            printf("Too chaotic\n");
+            return;
+        }
+    }
+    
+    printf("%i\n", ans);
+}
