934817c2cade7e2b84b115c46fb111a01aeb6dae
Ahmed Mohamed Fathy <ahmmohfat18@gmail.com>
2021-07-10 19:04:47 +0200

    Solve some Java problems.

---
+++ HackerRank/Dashboard/Java/Introduction/Java Output Formatting.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("================================");
+        
+        for (int i = 0; i < 3; ++i) {
+            
+            String s = scan.next();
+            int x = scan.nextInt();
+
+            System.out.printf("%-15s%03d%n", s, x);
+        }
+        
+        System.out.println("================================");
+    }
+}
