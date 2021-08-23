4bc65d740a0ef9f78a140e3b37589baead9327d9
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-08-23 08:46:58 +0200

    DAY 2 PROBLEM.

---
+++ HackerRank/Dashboard/Algorithms/Strings/Separate the Numbers.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+    int q;
+
+    std::cin >> q;
+
+    while (q--)
+    {
+        std::string s;
+
+        std::cin >> s;
+
+        int sSize = s.size();
+        bool isBeautiful = false;
+        long long prevNumber = -1;
+
+        for (size_t i = 1; i <= sSize / 2; ++i)
+        {
+            long long number = stol(s.substr(0, i));
+            prevNumber = number;
+
+            std::string test = std::to_string(number);
+            
+            while (test.size() < sSize)
+            {
+                test += std::to_string(++number);
+            }
+
+            if (test == s)
+            {
+                isBeautiful = true;
+                break;
+            }
+        }
+
+        if (isBeautiful)
+        {
+            std::cout << "YES " << prevNumber << '\n';
+        }
+        else
+        {
+            std::cout << "NO" << '\n';
+        }
+    }
+
+    return EXIT_SUCCESS;
+}
