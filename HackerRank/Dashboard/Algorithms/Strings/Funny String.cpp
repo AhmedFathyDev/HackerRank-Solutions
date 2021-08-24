fc7d079e5e7fde9ad172122887baa0839ea0445f
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-08-24 10:30:43 +0200

    Day 3 "Funny String" problem

---
+++ HackerRank/Dashboard/Algorithms/Strings/Funny String.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <algorithm>
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+    int q;
+
+    std::cin >> q;
+
+    while (q--)
+    {
+        std::string s, sReverse;
+
+        std::cin >> s;
+
+        sReverse = s;
+        reverse(sReverse.begin(), sReverse.end());
+
+        bool funny = true;
+        int sSize = s.size();
+
+        for (size_t i = 0; i < sSize - 1; ++i)
+        {
+            if (abs(s[i] - s[i + 1]) != abs(sReverse[i] - sReverse[i + 1]))
+            {
+                funny = false;
+                break;
+            }
+        }
+
+        std::cout << (funny ? "Funny" : "Not Funny") << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
