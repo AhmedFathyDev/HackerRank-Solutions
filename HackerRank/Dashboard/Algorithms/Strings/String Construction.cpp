ea46eb5d3807811b6a103f5811e8f6edef9a9a7f
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-09-01 22:58:35 +0200

    Day 11 "String Construction" challenge

---
+++ HackerRank/Dashboard/Algorithms/Strings/String Construction.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+    int n;
+
+    std::cin >> n;
+
+    while (n--)
+    {
+        std::string s;
+
+        std::cin >> s;
+
+        int min_cost = 0;
+        int s_size = s.size();
+        bool characters_frequency[26];
+
+        for (size_t i = 0; i < 26; ++i)
+        {
+            characters_frequency[i] = false;
+        }
+
+        for (size_t i = 0; i < s_size; ++i)
+        {
+            if (!characters_frequency[s[i] - 'a'])
+            {
+                min_cost++;
+            }
+            characters_frequency[s[i] - 'a'] = true;
+        }
+
+        std::cout << min_cost << '\n';
+    }
+
+    return EXIT_SUCCESS;
+}
