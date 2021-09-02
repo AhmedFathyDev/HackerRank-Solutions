5ade6f5082b27ffd52612da8fb89c00434bfa7b4
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-09-02 15:04:24 +0200

    Day 12 "Game of Thrones - I" challenge

---
+++ HackerRank/Dashboard/Algorithms/Strings/Game of Thrones - I.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+
+int characters_frequency[26];
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+    std::string s;
+
+    std::cin >> s;
+
+    int s_size = s.size(), odd_characters = 0;
+
+    for (size_t i = 0; i < s_size; ++i)
+    {
+        characters_frequency[s[i] - 'a']++;
+    }
+
+    for (size_t i = 0; i < 26; ++i)
+    {
+        if (characters_frequency[i] & 1)
+        {
+            odd_characters++;
+        }
+    }
+
+    std::cout << (odd_characters > 1 ? "NO" : "YES");
+
+    return EXIT_SUCCESS;
+}
