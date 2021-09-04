bd96f1ef272827081e1f21f09c256a244565c1db
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-09-04 03:29:32 +0200

    Solve 3 Codeforces problems

---
+++ Codeforces/Problemset/5A.cpp
@@ -0,0 +1,42 @@
+
+#include <vector>
+#include <iostream>
+
+int main(void)
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cout.tie(NULL);
+    std::cin.tie(NULL);
+
+	int p = 0, b = 0;
+
+	for (std::string s; getline(std::cin, s);)
+	{
+		switch (s[0])
+		{
+		case '+':
+			p++;
+			break;
+		case '-':
+			p--;
+			break;
+		default:
+			int l = 0;
+			bool f = 0;
+			for (char ch : s)
+			{
+				if (f)
+				{
+					l++;
+				}
+				if (ch == ':')
+				{
+					f = 1;
+				}
+			}
+			b += l * p;
+		}
+	}
+	
+	std::cout << b;
+}
---
+++ Codeforces/Problemset/6A.c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX(X, Y) ((X) < (Y) ? (Y) : (X))
+
+int main(void)
+{
+    int l[4];
+    bool seg = false;
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        scanf("%i", l + i);
+    }
+
+    for (size_t i = 0; i < 4; ++i)
+    {
+        for (size_t j = i + 1; j < 4; ++j)
+        {
+            for (size_t g = j + 1; g < 4; ++g)
+            {
+                int mx = MAX(l[i], MAX(l[j], l[g])), sum = l[i] + l[j] + l[g];
+
+                if (mx < sum - mx)
+                {
+                    printf("TRIANGLE");
+                    return EXIT_SUCCESS;
+                }
+                else if (mx == sum - mx)
+                {
+                    seg = true;
+                }
+            }
+        }
+    }
+
+    printf(seg ? "SEGMENT" : "IMPOSSIBLE");
+    return EXIT_SUCCESS;
+}
