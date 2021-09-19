400a467e8f204b4fc6c3836bc97a1affd89ca686
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-09-19 18:58:12 +0200

    Solve 2 SQL problems

---
+++ HackerRank/Dashboard/SQL/Advanced Select/The PADS.sql
@@ -0,0 +1,8 @@
+SELECT CONCAT(Name, '(', Substring(Occupation, 1, 1), ')') AS Name
+FROM OCCUPATIONS
+ORDER BY Name;
+
+SELECT CONCAT('There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.') AS Total
+FROM OCCUPATIONS
+GROUP BY Occupation
+ORDER BY Total;
---
+++ HackerRank/Dashboard/SQL/Aggregation/Weather Observation Station 2.sql
@@ -0,0 +1,2 @@
+SELECT FORMAT(Round(Sum(LAT_N), 2), '#.00'), FORMAT(Round(Sum(LONG_W), 2), '#.00')
+FROM STATION;
