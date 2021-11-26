551cb66432f9680c3976121170b586a36e944118
ahmedfathydev <ahmed.fathy.dev@gmail.com>
2021-11-26 14:14:30 +0200

    Update Solution.

--- HackerRank/Dashboard/SQL/Advanced Select/Type of Triangle.sql
+++ HackerRank/Dashboard/SQL/Advanced Select/Type of Triangle.sql
@@ -1,10 +1,10 @@
 SELECT CASE
-        WHEN 2 * GREATEST(A, B, C) >= (A + B + C) THEN "Not A Triangle"
-        WHEN A = B
-        AND A = C THEN "Equilateral"
-        WHEN A = B
-        OR A = C
-        OR B = C THEN "Isosceles"
+        WHEN 2 * GREATEST(A, B, C) >= (A + B + C)
+            THEN "Not A Triangle"
+        WHEN A = B AND A = C
+            THEN "Equilateral"
+        WHEN A = B OR A = C OR B = C
+            THEN "Isosceles"
         ELSE "Scalene"
     END
 FROM TRIANGLES;
